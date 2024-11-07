6d44fa6d8e763f303e3f163a7cc4a9c77f0561c2
NqobileBuhle <139454693+NqobileBuhle@users.noreply.github.com>
2024-11-07 15:45:43 +0200

    ..

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import { connectDB } from './config/db.js';
 import Product from './config/models/product.model.js';
+import mongoose from 'mongoose';
 
 dotenv.config();
 
@@ -39,6 +40,25 @@ app.post('/api/products', async (req, res) => {
         res.status(500).json({ success: false, message: "Server error" });
     }
 });
+
+//UPDATING A PRODUCT
+app.put("/api/products/:id",async(req,res)=>{
+    const{id}=req.params;
+    const product=req.body;
+    if(!mongoose.Types.ObjectId.isValid(id)){
+        return res.status(404).json({success:false,message:"Invalid Product id"});
+    }
+    try {
+       const updatedProduct= await Product.findByIdAndUpdate(id,product,{new:true});
+        res.status(200).json({success:true,data:updatedProduct});
+
+    } catch (error) {
+        console.log("error in deleting a product:",error.message);
+        res.status(500).json({success:false,message:"Product not found"});
+        
+    }
+
+})
 //Deleting a product
 app.delete("/api/products/:id",async(req,res)=>{
     const{id}=req.params;
